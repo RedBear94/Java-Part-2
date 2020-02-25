923458660a6ea6308fbab776e3e08f9f988a5068
Pavel <redbear95@gmail.com>
2020-02-25 14:11:15 +0300

    HomeWork2

---
+++ HomeWork2/src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.lang.reflect.Array;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String [][] myArray = new String[4][4];
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                myArray[i][j] = "1";
+            }
+        }
+        // myArray[3][0] = "A"; // NumberFormatException
+
+        try{
+            getArray4on4(myArray);
+        } catch (MyArraySizeException ex){
+            System.out.println(ex.getMessage());
+        } catch (NumberFormatException ex){
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public static void getArray4on4(String [][] arrayFourOnFour) throws MyArraySizeException {
+       int result = 0;
+       int[][] numbers = new int[4][4];
+       if (arrayFourOnFour[0].length != 4 || arrayFourOnFour.length != 4)
+           throw new MyArraySizeException("Размер массива должен быть 4 на 4", 1);
+       for(int i = 0; i < 4; i++){
+           for(int j = 0; j < 4; j++){
+               try {
+                   numbers[i][j] = Integer.parseInt(arrayFourOnFour[i][j]);
+                   result += Integer.parseInt(arrayFourOnFour[i][j]);
+               } catch (NumberFormatException ex){
+                   throw new MyArrayDataException(i, j);
+               }
+           }
+       }
+       System.out.println(result);
+    }
+}
